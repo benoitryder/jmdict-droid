7685f8913da744f9b7ff37865edb488345e58983
Benoît Ryder <benoit@ryder.fr>
2024-09-19 23:42:18 +0200

    Bump version, update metadata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "fr.ryder.benoit.jmdictdroid"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         vectorDrawables {
             useSupportLibrary = true
