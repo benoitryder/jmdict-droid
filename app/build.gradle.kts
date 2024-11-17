95e068398ffa6bcb766f94ea834e6317eb5be77e
Benoît Ryder <benoit@ryder.fr>
2024-11-17 22:25:19 +0200

    Bump version to 1.2, add changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "fr.ryder.benoit.jmdictdroid"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         vectorDrawables {
             useSupportLibrary = true
